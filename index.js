27cf174e54cdc65cf784e0df3c279043c0509942
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2018-12-12 20:31:16 +0000

    Fixes.

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ const mailOptions = {
 
 express()
   .post('/send', function (req, res) {
-    console.log("You are using the SMTP service " + SMTP_SERVICE + " and user account " + GMAIL_ACCOUNT);
-    console.log(GMAIL_PASSWORD);
     transporter.sendMail(mailOptions, function (err, info) {
-
        if(err) {
          console.log(err)
        }
@@ -34,8 +31,6 @@ express()
          console.log(info);
        }
     });
-
     res.end();
-
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
