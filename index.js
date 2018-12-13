d7930a9d15f223708e111ae7edb899c5fd9a97c0
Bruno Correia Caetano <bruno.correia.caetano@celfocus.com>
2018-12-13 14:19:11 +0000

    fixes for OAuth2

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
+let xoauth2 = require('xoauth2');
+let nodemailer = require('nodemailer');
+let smtpTransport = require('nodemailer-smtp-transport');
 
 const PORT = process.env.PORT || 3000;
-const GMAIL_ACCOUNT = process.env.GMAIL_ACCOUNT;
-const GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
+const GMAIL_ACCOUNT = process.env.GMAIL_ACCOUNT || 'brunoaccdev@gmail.com';
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_OAUTH2_REFRESH_TOKEN = process.env.GOOGLE_OAUTH2_REFRESH_TOKEN;
 const SMTP_SERVICE = process.env.SMTP_SERVICE || 'gmail';
 
-let transporter = nodemailer.createTransport({
- service: SMTP_SERVICE,
- port: 465,
- secure: true,
- auth: {
-        user: GMAIL_ACCOUNT,
-        pass: GMAIL_PASSWORD,
+let transporter = nodemailer.createTransport(
+  smtpTransport(
+    {  service: SMTP_SERVICE,
+    auth: {
+      xoauth2: xoauth2.createXOAuth2Generator(
+        {      user: GMAIL_ACCOUNT,
+        clientId: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        refreshToken: GOOGLE_OAUTH2_REFRESH_TOKEN
+        }
+      )
     }
-});
+  })
+);
+
 
 const mailOptions = {
-  from: 'brunoaccdev@gmail.com', // sender address
-  to: 'brunoaccdev@gmail.com', // list of receivers
+  from: GMAIL_ACCOUNT, // sender address
+  to: GMAIL_ACCOUNT, // list of receivers
   subject: 'Subject of your email', // Subject line
   html: 'Sent from express'// plain text body
 };
