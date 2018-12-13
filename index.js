acbb6bf273f37f6a3b732387f56c2700e7f4b03f
Bruno Correia Caetano <bruno.correia.caetano@celfocus.com>
2018-12-13 17:34:50 +0000

    fixes for OAuth2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 let xoauth2 = require('xoauth2');
 let nodemailer = require('nodemailer');
 let smtpTransport = require('nodemailer-smtp-transport');
+let bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 const GMAIL_ACCOUNT = process.env.GMAIL_ACCOUNT || 'brunoaccdev@gmail.com';
@@ -9,6 +10,8 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const GOOGLE_OAUTH2_REFRESH_TOKEN = process.env.GOOGLE_OAUTH2_REFRESH_TOKEN;
 const SMTP_SERVICE = process.env.SMTP_SERVICE || 'gmail';
+
+
 
 let transporter = nodemailer.createTransport(
   smtpTransport(
@@ -27,16 +30,25 @@ let transporter = nodemailer.createTransport(
 );
 
 
-const mailOptions = {
+let mailOptions = {
   from: GMAIL_ACCOUNT, // sender address
   to: GMAIL_ACCOUNT, // list of receivers
-  subject: 'Subject of your email', // Subject line
-  html: 'Sent from express'// plain text body
+  subject: '', // Subject line
+  html: ''// plain text body
 };
 
 express()
+  .use(bodyParser.json())
   .post('/send', function (req, res) {
+
+    mailOptions.subject = req.body.name + " | " + req.body.company + " | " + req.body.subject;
+    mailOptions.html = req.body.body;
+
+    console.log("Sending email...");
+    console.log(mailOptions);
+
     transporter.sendMail(mailOptions, function (err, info) {
+
        if(err) {
          console.log(err);
          res.status(500);
