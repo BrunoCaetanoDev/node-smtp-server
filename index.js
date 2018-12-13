1b2d3eae67253c585a85d6097bb3a16f56c9232e
Bruno Correia Caetano <bruno.correia.caetano@celfocus.com>
2018-12-13 16:47:37 +0000

    fixes for OAuth2

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let smtpTransport = require('nodemailer-smtp-transport');
 
 const PORT = process.env.PORT || 3000;
 const GMAIL_ACCOUNT = process.env.GMAIL_ACCOUNT || 'brunoaccdev@gmail.com';
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const GOOGLE_OAUTH2_REFRESH_TOKEN = process.env.GOOGLE_OAUTH2_REFRESH_TOKEN;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '266733661882-i9lgqhjjlgi7t3k7odti36ca0hdctg6b.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || 'UUlzhZ4CPb7vlKTtGskjYwLc';
+const GOOGLE_OAUTH2_REFRESH_TOKEN = process.env.GOOGLE_OAUTH2_REFRESH_TOKEN || '1/8on2A_apxrRvKVDg6mXm347DTbDDhcP6zsan4mC0hLM';
 const SMTP_SERVICE = process.env.SMTP_SERVICE || 'gmail';
 
 let transporter = nodemailer.createTransport(
@@ -37,10 +37,14 @@ express()
   .post('/send', function (req, res) {
     transporter.sendMail(mailOptions, function (err, info) {
        if(err) {
-         console.log(err)
+         console.log(err);
+         res.status(500);
+         res.send('Error sending email to recipients.');
        }
        else {
          console.log(info);
+         res.status(202);
+         res.send('Email sent to recipients.');
        }
     });
     res.end();
